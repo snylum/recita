cf688bc8dc25b8d48f7dde1a2f7a231400160c37
snylum <snylumagbas@gmail.com>
2025-09-21 13:54:09 +0800

    Update roster.js

--- functions/roster.js
+++ functions/roster.js
@@ -1,4 +1,4 @@
-import { getTeacherFromSession } from "../utils/session.js";
+import { getTeacherFromSession } from "./utils/session.js";
 
 export async function onRequest(context) {
   const { request, env } = context;
